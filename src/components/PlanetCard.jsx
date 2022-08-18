2227a8b5c3ef068607c3e645434c41b208506f3f
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-18 18:03:06 -0300

    requisito 6

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -13,6 +13,7 @@ class PlanetCard extends React.Component {
   }
 }
 PlanetCard.propTypes = {
+//   planetInfo: PropTypes.string.isRequired,
   planetName: PropTypes.string.isRequired,
   planetImage: PropTypes.string.isRequired,
 };
