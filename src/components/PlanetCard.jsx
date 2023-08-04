8aec7b3a338723bbd1b17dd198f913ba91d4351f
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-04 10:59:42 -0300

    css planets

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import '../styles/PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img className="planet" src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -3,18 +3,19 @@ import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
 
+import '../styles/SolarSystem.css';
+
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div className="solar-system">
         <Title headline="Planetas" />
-        {planets.map(
-          (
-            { name, image },
-          ) => (
-            <PlanetCard key={ name } planetName={ name } planetImage={ image } />
-          ),
-        )}
+        {planets.map(({ name, image }) => (
+          <div key={ name } className="planet-card">
+            <PlanetCard planetImage={ image } />
+            <div className="planet-name">{name}</div>
+          </div>
+        ))}
       </div>
     );
   }
